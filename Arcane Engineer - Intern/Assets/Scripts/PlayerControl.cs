b24cd31cb71e853d6c85a0197def9b57b30dda70
MichaelCourter <michael.courter.64@gmail.com>
2019-05-14 08:30:00 -0700

    Added the ElementAndInteractionPrototype scene. Implemented first person control functionality.
    ElementAndInteractionPrototype.unity: added to prototype the first person character, use of elements, and general interaction like zooming to a puzzle.
    
    Scripts.meta: since there wasn't anything in the folder, the meta data got updated, hopefully that doesn't mess with other collaborators' versions.
    
    CameraControl: added to let the attached camera follow a given gameobject at a given offset from the given gamobject's frame of reference. The y-axis is also based on the rotation of the given gameobject. The x-axis is based on axis input and a given sensitivity, and is limited by the given upper and lower bounds where 0 is 0 degrees and 1 is 360 degrees.
    
    PlayerControl: added to let the player move the Gameobject in first person. Takes horizontal and vertical (left, right and forward, backward) input from the given axes. Moves at a given speed. Rotates based on a given input axis (horizontal mouse movement) with a given sensitivity.
    
    ProjectVersion.txt: apparently the previous attempt at updating the version only added the Package/Manifest.json file and didn't update this one.

---
+++ Arcane Engineer - Intern/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] string horizontalName;
+    [SerializeField] string verticalName;
+    [SerializeField] float speed;
+    [SerializeField] string mouseXName;
+    [SerializeField] float horizontalSensitivity;
+
+    Rigidbody playersRigidBody;
+
+    // Use this for initialization
+    void Start () {
+        playersRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateMovement();
+        UpdateRotation();
+    }
+
+    void UpdateMovement()
+    {
+        float horizontalValue = Input.GetAxis(horizontalName);
+        float verticalValue = Input.GetAxis(verticalName);
+
+        if (horizontalValue != 0 || verticalValue != 0)
+        {
+            playersRigidBody.AddForce(transform.TransformPoint(new Vector3(horizontalValue * speed * Time.deltaTime, 0, verticalValue * speed * Time.deltaTime)));
+        }
+    }
+
+    void UpdateRotation()
+    {
+        // Adds the look input to the rotation of the camera.
+        Vector3 torque = new Vector3(0, Input.GetAxis(mouseXName) * horizontalSensitivity, 0);
+
+        playersRigidBody.AddTorque(torque * Time.deltaTime);
+    }
+}
