2cf35ee2bb9db5cb8dfa3aa6267c83d6cc73a1c9
MichaelCourter <michael.courter.64@gmail.com>
2019-05-15 20:59:41 -0700

    Added Soot and the water stream to clean it away.
    Soot.prefab: a plane who's sprite renderer's alpha goes down when affected by a water element.
    
    ElementAndInteractionPrototype.unity: added the SootGroup to showcase the water element affecting it. Added the
    
    Affected folder: holds scripts related to gameobjects that are affected by elements.
    
    ElementAffected: added to be the base class of any class that is affected by elements.
    
    Soot: added to make the Soot prefab disappear over time while it's being affected by the water element.
    
    Affectors folder: added to hold scripts related to gameobjects that call the Affect function of Affected classes.
    
    WaterStream: added to affect Affected gameobjects when they collide with the Gameobject.
    
    ElementList enum: added to represent all the elements, used to determin what element is being recieved by Affected().
    
    InteractableLever: changed the debug key to N.
    
    PlayerController: if the left mouse is clicked, the water stream is activated, otherwise it's disabled.
    
    TagManager.asset: added the Elements tag for OnTriggers to parse the correct colliders.

---
+++ Arcane Engineer - Intern/Assets/Scripts/Elements/Affectors/WaterStream.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterStream : MonoBehaviour {
+
+    [SerializeField] string affectedTag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay (Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == affectedTag)
+        {
+            other.GetComponent<ElementAffected>().Affect(Elements.water);
+        }
+    }
+}
