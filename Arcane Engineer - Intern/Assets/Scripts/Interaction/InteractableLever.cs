b23f44d3bf33a233ea90ec56617a675a9f00ff70
MichaelCourter <michael.courter.64@gmail.com>
2019-05-15 17:30:08 -0700

    Added a simple interactable lever. Moved camera code into the PlayerControl script.
    Lever.prefab: uses the InteractableLever script to act as a lever on a pedestal.
    
    ElementAndInteractionPrototype.unity: added the Lever prefab. Adjusted rigidbody and physics setting to have a slow fall again, but nicer xz movement.
    
    CameraControl: removed because the code was moved into the PlayerControl script.
    
    Interaction folder: added to hold scripts related Interaction functionality.
    
    Interactable: added as the base class for gameobjects that can be interacted with. Allows the raycaster to just call the universal function Interact().
    
    InteractableLever: added to be an interactable with a rotating piece that's rotated when switched on/off.
    
    PlayerControl: moved the CameraControl code into this script for understandability.
    
    TagManager.asset: added the Interaction layer that interactable gameobjects are on.

---
+++ Arcane Engineer - Intern/Assets/Scripts/Interaction/InteractableLever.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableLever : Interactable
+{
+    [SerializeField] GameObject leverRotationPoint;
+    [SerializeField] float onAngleDifference;
+    [SerializeField] float rotationSpeed;
+
+    float offAngle;
+    float onAngle;
+    bool isOn = false;
+
+	// Use this for initialization
+	void Start () {
+        // Assumes that the lever rotates on the x-axis when being turned on/off
+        offAngle = leverRotationPoint.transform.eulerAngles.x;
+        onAngle = offAngle - onAngleDifference;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Interact();
+        }
+	}
+
+    public override void Interact()
+    {
+        StartCoroutine(ShiftLever(!isOn));
+        isOn = !isOn;
+    }
+
+    IEnumerator ShiftLever(bool toOn)
+    {
+        float angleToReach = Mathf.Abs(onAngle - offAngle);
+        float currentRotated = 0;
+        int directionModifier = toOn ? -1 : 1;
+
+        while (currentRotated < angleToReach)
+        {
+            yield return new WaitForFixedUpdate();
+
+            leverRotationPoint.transform.eulerAngles += new Vector3(rotationSpeed * directionModifier, 0, 0);
+            currentRotated += rotationSpeed;
+        }
+        
+    }
+}
--- Arcane Engineer - Intern/Assets/Scripts/PlayerControl.cs
+++ Arcane Engineer - Intern/Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] GameObject cameraHolder;
+    [SerializeField] Vector3 cameraOffset = new Vector3(0, 0.5f, 0);
     [SerializeField] string horizontalName;
     [SerializeField] string verticalName;
     [SerializeField] float speed;
     [SerializeField] string mouseXName;
     [SerializeField] float horizontalSensitivity;
+    [SerializeField] string mouseYName;
+    [SerializeField] float verticalSensitivity = 1;
+    // Upper vertical angle limit.
+    [SerializeField] float upperVerticalAngleLimit = 0.15f;
+    // Lower vertical angle limit.
+    [SerializeField] float lowerVerticalAngleLimit = -0.15f;
+
+    [SerializeField] KeyCode interactKey = KeyCode.E;
+    [SerializeField] Vector3 interactionBoxHalfs;
+    [SerializeField] float interactionBoxTravelDistance;
+    [SerializeField] LayerMask interactionlayer;
+
+    [SerializeField] KeyCode elementUseKey = KeyCode.Mouse0;
 
     Rigidbody playersRigidBody;
+
+    float xDirection = 0;
 
     // Use this for initialization
     void Start () {
@@ -21,7 +38,21 @@ public class PlayerControl : MonoBehaviour
 	void Update ()
     {
         UpdateMovement();
-        UpdateRotation();
+        UpdateGameObjectRotation();
+        CheckForInteraction();
+
+        if (Input.GetKey(elementUseKey))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        cameraHolder.transform.position = transform.position + cameraOffset;
+
+        UpdateCameraRotation();
     }
 
     void UpdateMovement()
@@ -35,11 +66,45 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void UpdateRotation()
+    void UpdateGameObjectRotation()
     {
-        // Adds the look input to the rotation of the camera.
+        // Adds the look input to the rotation of the Gameobject.
         Vector3 torque = new Vector3(0, Input.GetAxis(mouseXName) * horizontalSensitivity, 0);
 
-        playersRigidBody.AddTorque(torque * Time.deltaTime);
+        playersRigidBody.MoveRotation(playersRigidBody.rotation * Quaternion.Euler(torque * Time.deltaTime));
+    }
+
+    void UpdateCameraRotation()
+    {
+        const short ANALOG_TO_DEGREES = 360;
+
+        // Adds the look input to the rotation of the camera.
+        xDirection += -Input.GetAxis(mouseYName) * verticalSensitivity * Time.deltaTime;
+
+        // If the absolute value of the x-axis rotation is greater than or equal to 1
+        if (Mathf.Abs(xDirection) >= 1)
+        {
+            // Set the ones place value of the value to 0 to loop the rotation.
+            xDirection = xDirection % 1;
+        }
+
+        // Limits the rotation in the x-axis to the given lower and upper limits.
+        xDirection = Mathf.Clamp(xDirection, lowerVerticalAngleLimit, upperVerticalAngleLimit);
+
+        cameraHolder.transform.eulerAngles = new Vector3(xDirection * ANALOG_TO_DEGREES, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    void CheckForInteraction()
+    {
+        if (Input.GetKeyDown(interactKey))
+        {
+            RaycastHit foundInteractable;
+            Physics.BoxCast(cameraHolder.transform.position, interactionBoxHalfs, cameraHolder.transform.TransformDirection(Vector3.forward), out foundInteractable, Quaternion.Euler(Vector3.forward), interactionBoxTravelDistance, interactionlayer.value);
+
+            if (foundInteractable.transform != null)
+            {
+                foundInteractable.transform.GetComponent<Interactable>().Interact();
+            }
+        }
     }
 }
