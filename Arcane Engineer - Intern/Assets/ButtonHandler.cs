580c66a133c10ca5333285300e35adb962fc7fda
travis ouellette <travis.brouellette@gmail.com>
2019-05-17 13:08:20 -0700

    Created reusable SelectionWheel
    Additions:
    - Meath font
    - Color changes
    - Support for right click interactability
    - Early support for selecting an element

---
+++ Arcane Engineer - Intern/Assets/ButtonHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MARK: - Class
+public class ButtonHandler : MonoBehaviour {
+	// MARK: Properties
+	enum Element { Water, Earth, Fire, Air }
+
+	Element selectedElement;
+
+	// MARK: Life Cycle
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// MARK: Private
+
+
+	// MARK: Public Control Handlers
+	// Determine the element and store
+	public void pickElement(int elementId) {
+		switch (elementId) {
+		case 0:
+			selectedElement = Element.Water;
+			break;
+		case 1:
+			selectedElement = Element.Earth;
+			break;
+		case 2:
+			selectedElement = Element.Fire;
+			break;
+		case 3:
+			selectedElement = Element.Air;
+			break;
+		default:
+			Debug.Log("Error: The ElementId of " + elementId + " is not a valid elementId");
+			return;
+		}
+
+		Debug.Log("You've successfully selected: " + selectedElement);
+	}
+}
--- Arcane Engineer - Intern/Assets/RightClickListener.cs
+++ Arcane Engineer - Intern/Assets/RightClickListener.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // MARK: - Class
 public class RightClickListener : MonoBehaviour {
 	// MARK: Properites
+	Image backgroundImage;
+	GameObject selectionWheel;
 
+	bool isRightClicking = false;
 
 	// MARK: Life Cycle
 	void Start () {
-		
+		if (GameObject.FindGameObjectWithTag ("BackgroundPanel")) { backgroundImage = GameObject.FindGameObjectWithTag("BackgroundPanel").GetComponent<Image>(); }
+		if (GameObject.FindGameObjectWithTag ("SelectionWheel")) { selectionWheel = GameObject.FindGameObjectWithTag("SelectionWheel"); }
 	}
 
 	void Update () {
-		
+   		if(Input.GetMouseButtonDown(1)) isRightClicking = true;
+   		if(Input.GetMouseButtonUp(1)) isRightClicking = false;
+
+   		if (isRightClicking) {
+			// Show selection wheel
+			Color c = backgroundImage.color;
+        	c.a = 0.75f;
+        	backgroundImage.color = c;
+        	selectionWheel.SetActive(true);
+		} else {
+			// Hide selection wheel
+			Color c = backgroundImage.color;
+        	c.a = 0f;
+        	backgroundImage.color = c;
+        	selectionWheel.SetActive(false);
+		}
 	}
 }
