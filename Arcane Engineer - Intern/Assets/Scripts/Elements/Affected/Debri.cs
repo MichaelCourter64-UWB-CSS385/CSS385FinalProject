6e91ffa5cb78cb732edc1807b9c97a5167344ca8
MichaelCourter <michael.courter.64@gmail.com>
2019-05-23 09:33:08 -0700

    Added Debri.
    Debri prefab: Is affected by the fire element.Deactivates self on Affect.
    
    FirstThirdOfRoom: added Debri prefabs next to the fireplace.
    
    Debri: deactivates self when affected by fire.
    
    FirePlume: basically a copy of WaterStream but this is seperate in case there's more functionality added like soot being placed where the fire collides.

---
+++ Arcane Engineer - Intern/Assets/Scripts/Elements/Affected/Debri.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debri : ElementAffected {
+    public override void Affect(Elements affectingElement)
+    {
+        if (affectingElement == Elements.Fire)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Arcane Engineer - Intern/Assets/Scripts/Elements/Affectors/FirePlume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlume : MonoBehaviour
+{
+    [SerializeField] string affectedTag;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == affectedTag)
+        {
+            other.GetComponent<ElementAffected>().Affect(Elements.Fire);
+        }
+    }
+}
