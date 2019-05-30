f7061f9ed704cb5f9a7648aae4f1957bec4b2e2a
MichaelCourter <michael.courter.64@gmail.com>
2019-05-29 21:48:26 -0700

    Started adding tutorial scenes. Cleaned up KeyDownInteractable.
    MovementTutorial scene: used to teach the player how to move and crouch.
    
    KeyDownInteractable: removed commented debug statements.

--- Arcane Engineer - Intern/Assets/Scripts/Interaction/KeyDownInteractable.cs
+++ Arcane Engineer - Intern/Assets/Scripts/Interaction/KeyDownInteractable.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public abstract class KeyDownInteractable : Interactable
 {
-    [SerializeField] float interactionDisableTime = 1;
     [SerializeField] GameObject objectToAnimate;
     [SerializeField] protected string animationStateName;
     [SerializeField] string neutralStateName;
@@ -23,24 +22,12 @@ public abstract class KeyDownInteractable : Interactable
 
     public override void Interact()
     {
-        Debug.Log("interactble: " + isInteractable);
         if (isInteractable)
         {
-            Debug.Log("button pressed at " + Time.time);
             isInteractable = false;
             ForInteract();
 
             StartCoroutine(WaitToAllowInteraction());
-            /*else
-            {
-                Debug.Log("re interact results:" + buttonAnimator.GetCurrentAnimatorStateInfo(0).IsName("Neutral") + ", " + !buttonAnimator.GetBool("isBeingPressed"));
-
-                if (buttonAnimator.GetCurrentAnimatorStateInfo(0).IsName("Neutral") && !buttonAnimator.GetBool("isBeingPressed"))
-                {
-                    Debug.Log("reset");
-                    isInteractable = true;
-                }// Baisically the animation controller takes a while to update. Consider calculating animation time based on speed to not reset intercatable until that time.
-            }*/
         }
     }
 
@@ -48,19 +35,13 @@ public abstract class KeyDownInteractable : Interactable
 
     IEnumerator WaitToAllowInteraction()
     {
-        //Debug.Log("start of wait:" + Time.time + ", speed:" + buttonAnimator.GetNextAnimatorStateInfo(0).speed);
-        //yield return new WaitForSeconds(interactionDisableTime);
-        //Debug.Log("end of wait:" + Time.time);
-
         while (!animator.GetCurrentAnimatorStateInfo(0).IsName(animationStateName))
         {
-            Debug.Log("waiting for anim");
             yield return new WaitForFixedUpdate();
         }
 
         while (!animator.GetCurrentAnimatorStateInfo(0).IsName(neutralStateName))
         {
-            Debug.Log("waiting for return to neutral");
             yield return new WaitForFixedUpdate();
         }
 
