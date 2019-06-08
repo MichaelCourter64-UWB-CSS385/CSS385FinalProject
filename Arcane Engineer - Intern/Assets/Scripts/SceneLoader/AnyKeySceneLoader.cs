dda9a33b536a132f85cd9c13f093f4fb5db311f8
MichaelCourter <michael.courter.64@gmail.com>
2019-06-07 21:14:15 -0700

    Added a splash screen.
    SplashImage image: used in the SplashScreen scene as the splash screen.
    
    SplashScreen scene: added to be the first scene after unity splash screen.
    
    ElementSelectionTutorial, ElementTutorial, InteractionTutorial, MovementTutorial scene: adjusted exit door serialized fields because of the addition to the ScenesList.
    
    AnyKeySceneLoader: if any key is pressed it loads the given scene.
    
    Scenes: added the SplashScreen scene to the list at the front.
    
    EditorBuildSettings asset: added the SplashScreen scene to the front of the build order.

---
+++ Arcane Engineer - Intern/Assets/Scripts/SceneLoader/AnyKeySceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnyKeySceneLoader : MonoBehaviour
+{
+    [SerializeField] ScenesList sceneToLoad;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.anyKey)
+        {
+            SceneManager.LoadSceneAsync((int)sceneToLoad);
+        }
+	}
+}
--- Arcane Engineer - Intern/Assets/Scripts/SceneLoader/Scenes.cs
+++ Arcane Engineer - Intern/Assets/Scripts/SceneLoader/Scenes.cs
@@ -1,5 +1,6 @@
 ﻿public enum ScenesList
 {
+    SplashScreen,
     MovementTutorial,
     InteractionTutorial,
     ElementTutorial,
