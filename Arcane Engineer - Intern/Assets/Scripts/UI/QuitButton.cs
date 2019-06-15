c5af1405a5dc9f87c92337f0a86ed7c8479ee122
MichaelCourter <michael.courter.64@gmail.com>
2019-06-14 17:24:31 -0700

    Improved end sequence. Made quit button accessible in every part of the game.
    EndScreen scene: adjusted anchors to scale UI correctly. Added a quit button.
    
    Playtest scene: added a midgame info screen that appears when the player enters the room behind the final door that informs that they're done for now. It then loads the EndScreen Scene. Added a quit button to the element selection screen.
    
    ElementSelectionTutorial, ElementTutorial, InteractionTutorial, MovementTutorial scene: added a quit button to the element selection screen.
    
    Splash meta: left over file that needed to be removed.
    
    QuitButton: adds a listener to a button on the Gameobject. When invoked, quits the application.

---
+++ Arcane Engineer - Intern/Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    [SerializeField] GameObject buttonHolder;
+
+    Button button;
+
+    void Awake()
+    {
+        button = buttonHolder.GetComponent<Button>();
+    }
+
+    void OnEnable()
+    {
+        button.onClick.AddListener(QuitGame);
+    }
+
+    void OnDisable()
+    {
+        button.onClick.RemoveListener(QuitGame);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
